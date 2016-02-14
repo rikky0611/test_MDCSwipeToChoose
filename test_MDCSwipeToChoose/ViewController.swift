47aed5ade45f3d449d9267280c44695262341034
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-02-14 10:50:46 +0900

    Initial Commit

---
+++ test_MDCSwipeToChoose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_MDCSwipeToChoose
+//
+//  Created by 荒川陸 on 2016/02/14.
+//  Copyright © 2016年 riku_arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ test_MDCSwipeToChooseTests/test_MDCSwipeToChooseTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_MDCSwipeToChooseTests.swift
+//  test_MDCSwipeToChooseTests
+//
+//  Created by 荒川陸 on 2016/02/14.
+//  Copyright © 2016年 riku_arakawa. All rights reserved.
+//
+
+import XCTest
+@testable import test_MDCSwipeToChoose
+
+class test_MDCSwipeToChooseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
